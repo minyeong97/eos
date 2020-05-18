09cd8f2dd0316de7f6a9823622e2d3c59431e284
manmarli <captivatemyj@gmail.com>
2020-05-18 19:10:17 +0900

    new

---
+++ core/task.c
@@ -0,0 +1,114 @@
+/********************************************************
+ * Filename: core/task.c
+ * 
+ * Author: parkjy, RTOSLab. SNU.
+ * 
+ * Description: task management.
+ ********************************************************/
+#include <core/eos.h>
+
+#define READY		1
+#define RUNNING		2
+#define WAITING		3
+
+/*
+ * Queue (list) of tasks that are ready to run.
+ */
+static _os_node_t *_os_ready_queue[LOWEST_PRIORITY + 1];
+
+/*
+ * Pointer to TCB of running task
+ */
+static eos_tcb_t *_os_current_task;
+
+int32u_t eos_create_task(eos_tcb_t *task, addr_t sblock_start, size_t sblock_size, void (*entry)(void *arg), void *arg, int32u_t priority) {
+	PRINT("task: 0x%x, priority: %d\n", (int32u_t)task, priority);
+	task->sp = _os_create_context(sblock_start, sblock_size, entry, arg);
+	task->priority = priority;
+	_os_node_t *new_node = malloc(sizeof(_os_node_t));
+	new_node->ptr_data = (void *)task;
+	new_node->priority = priority;
+	_os_add_node_tail(&(_os_ready_queue[priority]), new_node);
+}
+
+int32u_t eos_destroy_task(eos_tcb_t *task) {
+}
+
+void eos_schedule() {
+	if(_os_current_task != NULL)
+	{
+		addr_t sp = _os_save_context();
+		if(sp == NULL) return;
+
+		_os_current_task->sp = sp;
+		_os_node_t *new_node = malloc(sizeof(_os_node_t));
+		new_node->ptr_data = (void *)_os_current_task;
+		new_node->priority = _os_current_task->priority;
+		_os_add_node_tail(&(_os_ready_queue[_os_current_task->priority]), new_node);
+	}
+
+	int32u_t priority = _os_get_highest_priority();
+	_os_node_t *node = _os_ready_queue[priority];
+	_os_current_task = (eos_tcb_t*)(node->ptr_data);
+	_os_remove_node(_os_ready_queue, node);
+	free(node);
+		
+	_os_restore_context(_os_current_task->sp);
+}
+
+eos_tcb_t *eos_get_current_task() {
+	return _os_current_task;
+}
+
+void eos_change_priority(eos_tcb_t *task, int32u_t priority) {
+}
+
+int32u_t eos_get_priority(eos_tcb_t *task) {
+}
+
+void eos_set_period(eos_tcb_t *task, int32u_t period){
+	task->period = period;
+}
+
+int32u_t eos_get_period(eos_tcb_t *task) {
+}
+
+int32u_t eos_suspend_task(eos_tcb_t *task) {
+}
+
+int32u_t eos_resume_task(eos_tcb_t *task) {
+}
+
+void eos_sleep(int32u_t tick) {
+	extern system_timer;
+	eos_alarm_t *new_alarm = malloc(sizeof(struct eos_alarm_t));
+
+	eos_set_alarm(&system_timer, new_alarm, _os_current_task->period, _os_wakeup_sleeping_task(), _os_current_task);
+
+	eos_schedule();
+}
+
+void _os_init_task() {
+	PRINT("initializing task module.\n");
+
+	/* init current_task */
+	_os_current_task = NULL;
+
+	/* init multi-level ready_queue */
+	int32u_t i;
+	for (i = 0; i < LOWEST_PRIORITY; i++) {
+		_os_ready_queue[i] = NULL;
+	}
+}
+
+void _os_wait(_os_node_t **wait_queue) {
+}
+
+void _os_wakeup_single(_os_node_t **wait_queue, int32u_t queue_type) {
+}
+
+void _os_wakeup_all(_os_node_t **wait_queue, int32u_t queue_type) {
+}
+
+void _os_wakeup_sleeping_task(void *arg) {
+}
---
+++ hal/linux/context.c
@@ -0,0 +1,78 @@
+#include <core/eos.h>
+#include <core/eos_internal.h>
+#include "emulator_asm.h"
+
+typedef struct _os_context {
+	/* low address */
+	int32u_t edi;
+	int32u_t esi;
+	int32u_t ebx;
+	int32u_t edx;
+	int32u_t ecx;
+	int32u_t eax;
+	int32u_t eflags;
+	int32u_t entry;
+	int32u_t return_addr;
+	int32u_t arg;
+	/* high address */	
+} _os_context_t;
+
+void print_context(addr_t context) {
+	if(context == NULL) return;
+	_os_context_t *ctx = (_os_context_t *)context;
+	//PRINT("reg1  =0x%x\n", ctx->reg1);
+	//PRINT("reg2  =0x%x\n", ctx->reg2);
+	//PRINT("reg3  =0x%x\n", ctx->reg3);
+	//...
+}
+
+addr_t _os_create_context(addr_t stack_base, size_t stack_size, void (*entry)(void *), void *arg) {
+	_os_context_t *context = (_os_context_t*)(stack_base + stack_size/4 + 8);
+	context->arg = arg;
+	context->return_addr = NULL;
+	context->entry = entry;
+	context->eflags = 0;
+	context->eax = NULL;
+	context->ecx = NULL;
+	context->ebx = NULL;
+	context->edx = NULL;
+	context->esi = NULL;
+	context->edi = NULL;
+
+	return (addr_t)(context);
+}
+
+void _os_restore_context(addr_t sp) {
+	extern _eflags;
+	__asm__ __volatile__("\
+		movl %1, %%esp;\
+		popl %%edi;\
+		popl %%esi;\
+		popl %%ebx;\
+		popl %%edx;\
+		popl %%ecx;\
+		popl %%eax;\
+		popl %0;\
+		movl $0, %%eax;\
+		ret;"
+		:"=m"(_eflags):"m"(sp));
+}
+
+addr_t _os_save_context() {
+	extern _eflags;
+	__asm__ __volatile__("\
+		pushl $resume_eip;\
+		pushl $0;\
+		pushl %%eax;\
+		pushl %%ecx;\
+		pushl %%edx;\
+		pushl %%ebx;\
+		pushl %%esi;\
+		pushl %%edi;\
+		pushl 4(%%ebp);\
+		pushl (%%ebp);\
+		movl %%esp, %%eax;\
+		addl $8, %%eax;\
+	resume_eip:"
+		:: "r"(_eflags));
+}
